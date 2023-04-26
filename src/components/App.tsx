b6640653f52aa416c59389695209462ca38eee0a
Tianxing Wu <chi0chu319@gmail.com>
2023-04-26 22:30:19 +0800

    added favicon fixed language switch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,29 +5,40 @@ import MainScreen from "components/screens/main-screen";
 import ConfirmHandler from "components/contexts/confirm-handler";
 import ErrorHandler from "components/contexts/error-handler";
 import ApiProvider from "./providers/api-provider";
+import { useAppDispatch, useAppSelector } from "app/hooks";
+import { selectLocale, setLocale } from "features/locale-slice";
 
 const apiProviders: React.FC<{}>[] = [];
 
 /**
  * Application component
  */
-const App: React.FC = () => (
-  <ErrorHandler>
-    <ApiProvider providers={ apiProviders }>
-      <ConfirmHandler>
-        {/* <AccessTokenRefresh> */}
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={ <MainScreen/> }
-            />
-          </Routes>
-        </BrowserRouter>
-        {/* </AccessTokenRefresh> */}
-      </ConfirmHandler>
-    </ApiProvider>
-  </ErrorHandler>
-);
+const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { locale } = useAppSelector(selectLocale);
+
+  React.useLayoutEffect(() => {
+    dispatch(setLocale(locale));
+  }, []);
+
+  return (
+    <ErrorHandler>
+      <ApiProvider providers={ apiProviders }>
+        <ConfirmHandler>
+          {/* <AccessTokenRefresh> */}
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={ <MainScreen/> }
+              />
+            </Routes>
+          </BrowserRouter>
+          {/* </AccessTokenRefresh> */}
+        </ConfirmHandler>
+      </ApiProvider>
+    </ErrorHandler>
+  );
+};
 
 export default App;
